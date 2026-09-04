512126ca106d10270966c1c6a1df711a81375c13
Leonardo Maurício de Farias <furiossam@hotmail.com>
2026-09-04 16:22:21 -0300

    chore: estrutura inicial do projeto Labirinto Dungeonico

---
+++ backend/labirinto_dungeonico/src/main/java/com/furios/labirinto_dungeonico/LabirintoDungeonicoApplication.java
@@ -0,0 +1,13 @@
+package com.furios.labirinto_dungeonico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabirintoDungeonicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabirintoDungeonicoApplication.class, args);
+	}
+
+}
---
+++ backend/labirinto_dungeonico/src/test/java/com/furios/labirinto_dungeonico/LabirintoDungeonicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furios.labirinto_dungeonico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabirintoDungeonicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
